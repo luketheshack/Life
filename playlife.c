a7fa697171bfb2f4b25e048c3ebd1409723faa4a
luketheshack <lmarusha@student11.cse.nd.edu>
2020-10-06 14:44:28 -0400

    argc argv

--- playlife.c
+++ playlife.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include "lifefn.h"
+#include "lifefunc.h"
 
 const int MAX = 40;
 
 int main(int argc, char *argv[]) {
 	char current[MAX][MAX];
 	char temp[MAX][MAX];
+	printf("%d\n", argc);
+	return 0;
 }
