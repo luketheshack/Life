7f31beeea6755f9288be0d2fcfe9627c053621a0
luketheshack <lmarusha@nd.edu>
2020-10-07 15:54:35 -0400

    funcs and solve_cell

--- lifefunc.c
+++ lifefunc.c
@@ -6,3 +6,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lifefunc.h"
+
+void solve_cell(char temp[][maxsize], int row, int col) {
+	// conditionoal statements because edges, corner, center
+	// of grid must behave differently
+	int liveneighbors = 0, deadneighbors = 0;
+	int i, j;
+	for (i = -1; i <= 1; i++) {
+		for (j = -1; j <= 1; j++) {
+			if (!(row+i >= maxsize || col+i >= maxsize || row+i < 0 || col+i < 0 || (row == 0 && col == 0))) {
+				if (temp[row+i][col+i] == ' ') deadneighbors++;
+				if (temp[row+i][col+i] == 'X') liveneighbors++;
+			}
+		}
+	}
+	if (temp[row][col] == ' ' && liveneighbors == 3) return 'X';
+	if (temp[row][col] == 'X' && !(liveneighbors == 2 || liveneighbors == 3) return ' ';
+	else return temp[row][col];
+}
+
+void advance_simulation(char real[][maxsize], char temp[][maxsize]) {
+	int i, j;
+	
+}
--- lifefunc.h
+++ lifefunc.h
@@ -2,4 +2,9 @@
 // Fundamentals of Computing : Lab #7
 // lifefunc.h
 
-
+const int maxsize = 40;
+int show_input();
+void add_live_cell(char [][maxsize], int row, int col);
+void make_dead_cell(char [][maxsize], int row, int col);
+void advance_simulation(char [][maxsize], char [][maxsize]);
+char solve_cell(char [][maxsize], int row, int col);
