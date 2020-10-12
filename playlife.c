4076b683f8b560316ade9f6c8d7d9de645427ec0
luketheshack <lmarusha@nd.edu>
2020-10-12 09:38:08 -0400

    bug fixing

--- playlife.c
+++ playlife.c
@@ -115,7 +115,7 @@ int main(int argc, char *argv[]) {
 			// advance simulation
 			advance_simulation(real, temp);
 			// wait
-			usleep(250000);
+			usleep(25000000);
 			// clear screen
 			system("clear");
 		}
